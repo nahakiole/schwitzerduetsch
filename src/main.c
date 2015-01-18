d5207ca587cd269d25bb8819b3a681f5e483ed4a
Robin Glauser <robin.glauser@gmail.com>
2015-01-18 14:54:22 +0100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,94 @@
+#include <pebble.h>
+  
+static Window *s_main_window;
+static TextLayer *s_hour_layer;
+static TextLayer *s_min_layer;
+
+static char hours[13][7] = {"nuu", "eis", "zwöi", "drü", "vieri", "füfi", "sächis", "sibni", "achti", "nüni", "zäni", "eufi", "zwöufi"};
+
+static char special[12][20] = {"öpä","füf ab","zäh ab","viertu ab", "zwänzg ab", "füfäzwängsab", "haubi", "füf ab haubi", "zwänzg vor", "viertu vor", "zäh vor", "füf vor"};
+
+
+static int get_aprox_minute(int minute) {
+		return ((minute+2)/5);
+}
+
+static char *get_minute_string(int minute) {
+		return special[minute];
+}
+
+static void update_time() {
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+  int hour = tick_time->tm_hour;
+	int minute = get_aprox_minute(tick_time->tm_min);
+	if (minute > 7){
+		hour++;
+	}
+	
+  text_layer_set_text(s_min_layer, get_minute_string(minute));
+  text_layer_set_text(s_hour_layer, hours[hour%12]);
+}
+
+
+
+static void main_window_load(Window *window) {
+  s_hour_layer = text_layer_create(GRect(0, 80, 144, 50));
+	
+  s_min_layer = text_layer_create(GRect(0, 40, 144, 50));
+	
+	text_layer_set_background_color(s_min_layer, GColorClear);
+  text_layer_set_text_color(s_min_layer, GColorWhite);
+	
+	text_layer_set_background_color(s_hour_layer, GColorClear);
+  text_layer_set_text_color(s_hour_layer, GColorWhite);
+	
+  text_layer_set_text(s_hour_layer, hours[0]);
+	
+  text_layer_set_text(s_min_layer, special[0]);
+	
+	
+  window_set_background_color(window, GColorBlack);
+
+  text_layer_set_font(s_hour_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_hour_layer, GTextAlignmentCenter);
+	
+	 text_layer_set_font(s_min_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+  text_layer_set_text_alignment(s_min_layer, GTextAlignmentCenter);
+
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_hour_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_min_layer));
+  update_time();
+}
+
+static void main_window_unload(Window *window) {
+  text_layer_destroy(s_hour_layer);
+  text_layer_destroy(s_min_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+  
+static void init() {
+  s_main_window = window_create();
+
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  window_stack_push(s_main_window, true);
+  
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+}
+
+static void deinit() {
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
